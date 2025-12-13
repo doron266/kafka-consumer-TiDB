f41842d5aa3fbd08e6b949cb74d1480ee47e9c81
doron266 <doronw90@gmail.com>
2025-12-13 19:13:39 +0200

    Update init-ticdc.sh

--- init-ticdc.sh
+++ init-ticdc.sh
@@ -17,7 +17,8 @@ sleep 45
 echo "[TiCDC] Creating changefeed 'db-monitor'..."
 /cdc cli changefeed create \
   --sink-uri='kafka://kafka:9092/tests?protocol=canal-json&kafka-version=2.4.0&partition-num=6&max-message-bytes=67108864&replication-factor=1' \
-  --changefeed-id="db-monitor" || \/cdc cli changefeed update \
+  --changefeed-id="db-monitor" || \
+  /cdc cli changefeed update \
   --sink-uri='kafka://kafka:9092/tests?protocol=canal-json&kafka-version=2.4.0&partition-num=6&max-message-bytes=67108864&replication-factor=1' \
   --changefeed-id="db-monitor"
   echo "[TiCDC] eror occured during setup."
