aa0d114fb17d0887c2b5b1711844b9aca8cb513f
doron266 <doronw90@gmail.com>
2025-12-14 14:29:41 +0200

    Update init-ticdc.sh

--- init-ticdc.sh
+++ init-ticdc.sh
@@ -2,24 +2,24 @@
 set -e
 
 
-echo "[TiCDC] Waiting for TiCDC to be ready..."
+echo "[cdc-task] Waiting for TiCDC to be ready..."
 sleep 45
 
-echo "[TiCDC] Creating changefeed 'db-monitor'..."
+echo "[cdc-task] Creating changefeeds [tests, users, orders]..."
 /cdc cli changefeed create \
   --server=http://ticdc:8300 \
   --sink-uri='kafka://kafka:9092/tests?protocol=open-protocol' \
-  --changefeed-id="tests"
+  --changefeed-id="tests"\
   --filter='mydb.tests' || echo "[cdc-task] [changefeed: tests, probably exist]"
   /cdc cli changefeed create \
   --server=http://ticdc:8300 \
   --sink-uri='kafka://kafka:9092/orders?protocol=open-protocol' \
-  --changefeed-id="orders"
+  --changefeed-id="orders"\
   --filter='mydb.orders' || echo "[cdc-task] [changefeed: orders, probably exist]"
   /cdc cli changefeed create \
     --server=http://ticdc:8300 \
   --sink-uri='kafka://kafka:9092/users?protocol=open-protocol' \
-  --changefeed-id="users"
+  --changefeed-id="users"\
   --filter='mydb.users' || echo "[cdc-task] [changefeed: users, probably exist]"
   echo "[TiCDC-task] [exit code: 0, massage: changefeeds created succesfully]" || echo "exit code 1"
   
