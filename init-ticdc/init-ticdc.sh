793dd7903bbbb3cff83e14b2f5ac3331b016ff02
doron266 <doronw90@gmail.com>
2025-12-14 20:17:07 +0200

    Update init-ticdc.sh

--- init-ticdc/init-ticdc.sh
+++ init-ticdc/init-ticdc.sh
@@ -8,9 +8,9 @@ sleep 45
 echo "[cdc-task] Creating changefeeds [tests, users, orders]..."
 /cdc cli changefeed create \
   --server=http://ticdc:8300 \
-  --sink-uri='kafka://kafka:9092/tests?protocol=open-protocol' \
-  --changefeed-id="tests"\
-  --config /init-ticdc/ticdc-User-changefeed.toml || echo "[cdc-task] [changefeed: tests, probably exist]"
+  --sink-uri='kafka://kafka:9092/users?protocol=open-protocol' \
+  --changefeed-id="users"\
+  --config /init-ticdc/ticdc-User-changefeed.toml || echo "[cdc-task] [changefeed: usesr, probably exist]"
   /cdc cli changefeed create \
   --server=http://ticdc:8300 \
   --sink-uri='kafka://kafka:9092/orders?protocol=open-protocol' \
