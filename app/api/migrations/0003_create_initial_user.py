73559af127cd46b7a7320dd5f6ac25615a8408d2
doron266 <doronw90@gmail.com>
2025-12-15 10:53:46 +0200

    Create 0003_create_initial_user.py

---
+++ app/api/migrations/0003_create_initial_user.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+
+def create_initial_user(apps, schema_editor):
+    User = apps.get_model("api", "User")
+
+    User.objects.get_or_create(
+        email="admin@example.com",
+        defaults={
+            "username": "doron",
+            "password": "doron2002",
+            "auth_token": None,
+        },
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("api", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_initial_user),
+    ]
