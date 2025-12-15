8c211deffe41ad1ea3f920856f9e8ede8cbed928
doron266 <doronw90@gmail.com>
2025-12-15 10:26:46 +0200

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS api_user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL, -- In production, store hashed passwords!
+    password VARCHAR(255) NOT NULL,
     auth_token VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
