ec77725fcbe8492da69bcd4e81a92c358c94750a
doron266 <doronw90@gmail.com>
2025-12-15 11:14:10 +0200

    Update urls.py

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Users
     path("users", views.get_users, name="get_users"),
     path("users/add", views.add_user, name="add_user"),
     path("users/update", views.update_user, name="update_user"),
     path("users/delete", views.delete_user, name="delete_user"),
-    path("orders", views.get_orders, name="get_orders"),
-    path("orders/add", views.add_order, name="add_order"),
-    path("orders/update", views.update_order, name="update_order"),
-    path("orders/delete", views.delete_order, name="delete_order"),
+
+    # Logins
+    path("logins", views.get_logins, name="get_logins"),
+    path("logins/add", views.add_login, name="add_login"),
+    path("logins/delete", views.delete_logins_by_email, name="delete_logins_by_email"),
 ]
