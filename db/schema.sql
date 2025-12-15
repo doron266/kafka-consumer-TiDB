45888b7b28a11cba4b57fee2289461ac489d52c4
doron266 <doronw90@gmail.com>
2025-12-15 10:42:21 +0200

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,13 +2,3 @@ CREATE DATABASE IF NOT EXISTS mydb;
 USE mydb;
 
 
-CREATE TABLE IF NOT EXISTS api_user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    auth_token VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-INSERT INTO api_user (username, email, password) VALUES ('user', 'doron@example.com', 'doron')
-ON DUPLICATE KEY UPDATE password='admin';
